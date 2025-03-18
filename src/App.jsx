6c5b90915feea9dbca1ddcde7e88576fb5adb899
FredClark <wolfbane6758@gmail.com>
2025-03-17 21:45:24 -0400

    Create new folders, add CartQuantity

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import PLANTS from "./data";
+
 export default function App() {
-  return <></>;
+  const plants = useState(PLANTS[0]);
+  const [selectedPlant, setSelectedPlant] = useState();
+  
+  
+
+  return (
+  <>
+    <header>Proper Plants</header>
+    <h1>Plants</h1>
+    <section className="plant">
+      {plants.image}
+      {plants.name}
+      <button>Add to cart</button>
+    </section>
+    <section className="cart">
+      <h1>Cart</h1>
+      {plants.image}
+      {plants.name}
+      <button>-</button>
+      <p></p>
+      <button>+</button>
+    </section>
+  </>
+  );
 }
